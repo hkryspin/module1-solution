8438e7665380dd6d94a39c67d4010a7d19b40dca
hkryspin <hkryspin@gmail.com>
2017-04-03 23:27:33 +0200

    Final.

---
+++ app.js
@@ -0,0 +1,29 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+
+  $scope.dishes = "";
+
+  $scope.checkLunch = function () {
+    var dishCount = $scope.dishes.split(',');
+    switch (true) {
+      case (dishCount >= 1 && dishCount <= 3):
+        $scope.message = "Enjoy!";
+        break;
+      case (dishCount > 3):
+        $scope.message = "Too much!";
+        break;
+      case (dishCount == ""):
+        $scope.message = 'Please enter data first';
+        break;
+      default:
+    }
+  }
+};
+
+})();
